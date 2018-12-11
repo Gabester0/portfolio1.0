f3cd5bd27b2ac14517d3ca3b1f35fd4c1ec619c2
Gabester0 <gceipper@gmail.com>
2018-12-10 23:24:26 -0500

    Basic planning for the js file, added classes to sort selections to the index.html file

---
+++ script.js
@@ -0,0 +1,14 @@
+//FCC, FE, PHP, JS, courseProject, techDoc, Pro, API
+
+//Need an eventListener for the select to initiate the script
+//Will set the current selection
+
+//Then the js will add an inactive class to everything not selected
+//The default state will be that all projects are active.
+let itemClasses = ''; //All the coursework items (JS object with the classes)
+//This should contain all projects with each project including all classes in a js object
+
+//then we use a for in loop to iterate through and add an inactive css class to all that don't
+//contain the current selection.  
+
+//The css inactive class will make these projects display off screen.  
