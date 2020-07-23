8064f5d91dcc3cbe34afcb420420e5b3f86f6f9c
Gabester0 <gceipper@gmail.com>
2020-07-22 20:28:54 -0400

    Most revisions are complete

--- script.js
+++ script.js
@@ -1,49 +1,35 @@
-//Skills Icons Hover Effects
+// //Skills Icons Hover Effects
 
-const skillIcons = document.querySelectorAll(`.skill`);
-const iconArray = Array.from(skillIcons);
-const skillLabels = document.querySelectorAll(`.skillLabel`);
-const labelArray = Array.from(skillLabels);
+// const skillIcons = document.querySelectorAll(`.skill`);
+// const iconArray = Array.from(skillIcons);
+// const skillLabels = document.querySelectorAll(`.skillLabel`);
+// const labelArray = Array.from(skillLabels);
 
-iconArray.forEach((icon)=>{
-    icon.addEventListener(`mouseover`, ()=>{
-        let index = iconArray.indexOf(icon);
-        labelArray[index].style.opacity = 1;
-        icon.style.opacity = .7;
-        icon.style.transform = "translateY(1px)";
-    });
-    icon.addEventListener(`mouseout`, ()=>{
-        let index = iconArray.indexOf(icon);
-        labelArray[index].style.opacity = 0;
-        icon.style.opacity = 1;
-        icon.style.transform = "translateY(0px)";
-    })
-});
+// iconArray.forEach((icon)=>{
+//     icon.addEventListener(`mouseover`, ()=>{
+//         let index = iconArray.indexOf(icon);
+//         labelArray[index].style.opacity = 1;
+//         icon.style.opacity = .7;
+//         icon.style.transform = "translateY(1px)";
+//     });
+//     icon.addEventListener(`mouseout`, ()=>{
+//         let index = iconArray.indexOf(icon);
+//         labelArray[index].style.opacity = 0;
+//         icon.style.opacity = 1;
+//         icon.style.transform = "translateY(0px)";
+//     })
+// });
 
 
-const classes = [`HTML`, `CSS`, `JAVASCRIPT`, `REACT`, `GIT`,`BOOTSTRAP`, `SASS`];
+// const classes = [`HTML`, `CSS`, `JAVASCRIPT`, `REACT`, `GIT`,`BOOTSTRAP`, `SASS`];
 
-// let dropdown = document.getElementById(`select`);
+// // let dropdown = document.getElementById(`select`);
 
-iconArray.forEach(icon=>{
-    icon.addEventListener(`click`, ()=>{
-        let currentSelection = icon.id;
-        console.log(currentSelection);
-        classes.forEach(cl =>{
-            let currentCl = document.querySelectorAll(`.${cl}`);
-            currentCl.forEach(element =>{
-                if(! element.classList.contains(currentSelection)){
-                    element.classList.add(`inactive`);
-                } else {
-                    element.classList.remove(`inactive`);
-                }
-            })
-        });
-    })
-})
-// dropdown.addEventListener(`change`, ()=>{
-//     let currentSelection = dropdown.value;
-//     classes.forEach(cl =>{
+// iconArray.forEach(icon=>{
+//     icon.addEventListener(`click`, ()=>{
+//         let currentSelection = icon.id;
+//         console.log(currentSelection);
+//         classes.forEach(cl =>{
 //             let currentCl = document.querySelectorAll(`.${cl}`);
 //             currentCl.forEach(element =>{
 //                 if(! element.classList.contains(currentSelection)){
@@ -51,6 +37,7 @@ iconArray.forEach(icon=>{
 //                 } else {
 //                     element.classList.remove(`inactive`);
 //                 }
-//            })
-//     });
-// });
+//             })
+//         });
+//     })
+// })
